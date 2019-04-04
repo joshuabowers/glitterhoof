83b1a23ed991d21242a4dda0091587effb07590e
Joshua Bowers <joshua.bowers+code@gmail.com>
2019-04-04 14:36:22 -0700

    Added in an icon for the site header.

--- app/javascript/src/glitterhoof/about/about-component.jsx
+++ app/javascript/src/glitterhoof/about/about-component.jsx
@@ -9,6 +9,11 @@ const AboutComponent = (props) => (
       <ul>
         <li><a href='https://github.com/qrohlf/trianglify'>Trianglify</a></li>
         <li><a href='https://fonts.google.com/specimen/UnifrakturCook'>UnifrakturCook</a></li>
+        <li>
+          Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a>&nbsp;
+          from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>&nbsp;
+          is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+        </li>
       </ul>
     </nav>
   </footer>
--- app/javascript/src/glitterhoof/site-header/site-header-component.jsx
+++ app/javascript/src/glitterhoof/site-header/site-header-component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from './styles';
+import horseIcon from './horse-standing-on-three-paws-black-shape-of-side-view.svg';
 
 const SiteHeaderComponent = (props) => (
   <header className={ styles.siteHeader }>
+    <img src={ horseIcon } />
     <h1>Glitterhoof</h1>
   </header>
 );
