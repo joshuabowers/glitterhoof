8aef1b6f309d2c147e22272a895280e4351efc91
Joshua Bowers <joshua.bowers+code@gmail.com>
2019-04-23 13:18:15 -0700

    Attempting to fix issue with ActionCable / CableCar on heroku.

--- app/javascript/src/sagas/index.js
+++ app/javascript/src/sagas/index.js
@@ -13,8 +13,8 @@ export function* rootSaga() {
     helloSaga(),
     createChronicle(),
     analyzeChronicle(),
-    hydrate(),
-    hydrateChronicle()
+    hydrateChronicle(),
+    hydrate()
   ]);
 }
 
