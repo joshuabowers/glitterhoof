07bc9af701f80a978cc00c8880837f0ea09e7416
Joshua Bowers <joshua.bowers+code@gmail.com>
2019-04-24 16:26:57 -0700

    Fairly simple implementation of the redux-actioncable middleware: should be feature complete for Glitterhoof, but decidedly lacking features to work in other contexts which require extra behavior.

--- app/javascript/src/redux-actioncable.js
+++ app/javascript/src/redux-actioncable.js
@@ -1,16 +1,16 @@
 import ActionCable from 'actioncable';
 
 const cable = store => {
-  let consumer = null, subscription = null, options = {};
+  let consumer = null, subscription = null, options = {}, prefix = 'RAILS_',
+      isPrefix = null;
 
   cable.connect = (channel, { cableParams = {}, ...config }) => {
     options = config;
     const params = { channel: channel, ...cableParams };
+    prefix = options.prefix || prefix;
+    isPrefix = new RegExp( `^${ prefix }` );
 
     consumer = ActionCable.createConsumer( config.url );
-
-    console.log( consumer );
-
     subscription = consumer.subscriptions.create( params, {
       initialized: () => {
         store.dispatch( { type: 'CABLE_INITIALIZED' } );
@@ -19,19 +19,26 @@ const cable = store => {
         store.dispatch( { type: 'CABLE_CONNECTED' } );
       },
       disconnected: () => {
-
+        store.dispatch( { type: 'CABLE_DISCONNECTED' } );
       },
       received: (message) => {
         store.dispatch( message );
       },
       rejected: () => {
-
+        throw new Error('redux-actioncable: connection rejected');
       }
     } );
   };
 
   return next => action => {
-    return next( action );
+    const { type } = action;
+
+    if( type.match( isPrefix ) ){
+      subscription.send( action );
+      return store.getState();
+    } else {
+      return next( action );
+    }
   };
 };
 
