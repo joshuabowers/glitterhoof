46a73dbc6fffffd23f40ad03c35ffd6b8cb2a214
Joshua Bowers <joshua.bowers+code@gmail.com>
2019-04-24 15:37:04 -0700

    Configuration issue: production was not enforcing SSL, and was accepting unsecured cable requests but not secure cable requests.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,10 +41,10 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://glitterhoof-staging.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://glitterhoof-staging.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = [ 'https://glitterhoof-staging.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
